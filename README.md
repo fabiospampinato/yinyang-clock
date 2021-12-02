55504acea4c1ea5c2518daf69b565c19738273e4
Fabio Spampinato <spampinabio@gmail.com>
2021-12-02 15:06:49 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+<p align="center">
+  <img src="resources/screenshot.png" width="600">
+</p>
+
+# YinYang Clock
+
+A clock that keeps track of time spent yinning vs time spent yanging.
+
+It's basically just a bidirectional stopwatch, each time you click it the current side is paused and the other side is resumed.
+
+## License
+
+MIT © Fabio Spampinato
---
+++ src/index.js
@@ -0,0 +1,171 @@
+
+/* VARIABLES */
+
+const $APP = document.getElementById ( 'app' );
+const $GOOD = document.getElementById ( 'good' );
+const $BAD = document.getElementById ( 'bad' );
+const $THRESHOLD = document.getElementById ( 'threshold' );
+const $CLOCK = document.getElementById ( 'clock' );
+
+const DATABASE_NAMESPACE = 'yyc';
+const START_AT_MIDNIGHT = false;
+
+/* HELPERS */
+
+const getTimestampAtMidnight = () => {
+
+  const date = new Date ();
+
+  date.setHours ( 0, 0, 0, 0 );
+
+  return date.getTime ();
+
+};
+
+const getTimestampAtPreset = () => {
+
+  return Date.now ();
+
+};
+
+const getMillisecondsSinceMidnight = () => {
+
+  return getTimestampAtPreset () - getTimestampAtMidnight ();
+
+};
+
+const getClock = seconds => {
+
+  const h = Math.floor ( ( seconds / ( 60 * 60 ) ) );
+  const m = Math.floor ( ( seconds / 60 ) % 60 );
+  const s = Math.floor ( seconds % 60 );
+
+  return [h, m, s].map ( nr => String ( nr ).padStart ( 2, '0' ) ).join ( ':' );
+
+};
+
+const getRatio = () => {
+
+  return Math.min ( 1, Math.max ( 0, ( State.goodMs () / ( State.goodMs () + State.badMs () ) ) ) );
+
+};
+
+const updateClass = () => {
+
+  $APP.classList.toggle ( 'is-good', State.good () );
+  $APP.classList.toggle ( 'is-bad', !State.good () );
+
+};
+
+const updateClock = () => {
+
+  const milliseconds = State.good () ? State.goodMs () : State.badMs ();
+  const seconds = Math.floor ( milliseconds / 1000 );
+  const clock = getClock ( seconds );
+
+  $CLOCK.textContent = clock;
+
+};
+
+const updateTimings = () => {
+
+  const presentTimestamp = getTimestampAtPreset ();
+  const elapsedMs = presentTimestamp - State.updateTimestamp ();
+  const bucket = State[State.good () ? 'goodMs' : 'badMs'];
+
+  bucket ( bucket () + elapsedMs );
+
+  State.updateTimestamp ( presentTimestamp );
+
+};
+
+const updateGood = () => {
+
+  $GOOD.style.flexGrow = getRatio ();
+
+};
+
+const updateBad = () => {
+
+  $BAD.style.flexGrow = 1 - getRatio ();
+
+};
+
+const updateReset = () => {
+
+  if ( !START_AT_MIDNIGHT ) return;
+
+  if ( ( State.goodMs () + State.badMs () ) < 86_400_000 ) return;
+
+  State.good ( false );
+  State.goodMs ( 0 );
+  State.badMs ( 0 );
+
+};
+
+const update = () => {
+
+  updateTimings ();
+  updateGood ();
+  updateBad ();
+  updateClass ();
+  updateClock ();
+  updateReset ();
+
+};
+
+const flip = () => {
+
+  update ();
+
+  State.good ( !State.good () );
+
+  update ();
+
+};
+
+/* DATABASE */
+
+const Database = {
+  read: ( key, fallback ) => {
+    try {
+      return JSON.parse ( localStorage.getItem ( `${DATABASE_NAMESPACE}.${key}` ) || '' );
+    } catch {
+      Database.write ( key, fallback );
+      return fallback;
+    }
+  },
+  write: ( key, value ) => {
+    return localStorage.setItem ( `${DATABASE_NAMESPACE}.${key}`, JSON.stringify ( value ) );
+  },
+  observable: ( key, fallback ) => {
+    return function ( valueNext ) {
+      if ( arguments.length ) {
+        return Database.write ( key, valueNext );
+      } else {
+        return Database.read ( key, fallback );
+      }
+    };
+  }
+};
+
+/* STATE */
+
+const State = {
+  good: Database.observable ( 'good', false ),
+  goodMs: Database.observable ( 'goodMs', 0 ),
+  badMs: Database.observable ( 'badMs', START_AT_MIDNIGHT ? getMillisecondsSinceMidnight () : 0 ),
+  updateTimestamp: Database.observable ( 'updateTimestamp', Date.now () )
+};
+
+/* INIT */
+
+update ();
+
+setInterval ( update, 1000 );
+
+$APP.addEventListener ( 'click', flip );
+
+navigator.wakeLock.request ( 'screen' );
+
+// window.onbeforeunload = () => 'Progress may be lost!';
